dea1d6448c651464f03654cd9f20274b971fb3a8
Eironeia <acuellor.6@gmail.com>
2021-04-30 11:22:49 +0200

    first commit

---
+++ Gymber/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Gymber
+//
+//  Created by Alex Cuello on 30/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
